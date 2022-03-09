e13c8c44b894b79b0a5f135d8eaa8cc7a1d89525
sanevillain <denis.ismail@code.berlin>
2022-03-09 16:14:39 +0100

    Add user model, controller and resource

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,11 @@
+class UserResource < JSONAPI::Resource
+  attributes :email, :password, :password_confirmation
+
+  def fetchable_fields
+    [:email]
+  end
+
+  def self.creatable_fields(_context)
+    %i[email password password_confirmation]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'hello#hello'
+
+  jsonapi_resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
