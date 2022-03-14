a6f20eedd634b4143dac89151154cdab0fed8f53
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2022-03-14 20:34:42 +0100

    Add refresh_token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_163427) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_14_191219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_163427) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "refresh_token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["refresh_token"], name: "index_users_on_refresh_token"
   end
 
 end
