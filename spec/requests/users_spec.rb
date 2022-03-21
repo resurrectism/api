dc327859fe80341adbc7d96adc6032b13046302f
sanevillain <denis.ismail@code.berlin>
2022-03-21 16:08:22 +0100

    Tests: Add test for Users#create

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /profile' do
-    subject { response }
+  subject { response }
 
+  describe 'POST /users' do
+    let(:attributes) { {} }
+
+    before do
+      headers = { 'Content-Type' => 'application/vnd.api+json' }
+      params = { data: { type: 'users', attributes: attributes } }
+      post users_path, headers: headers, params: params.to_json
+    end
+
+    context 'when called without an email' do
+      it { is_expected.to have_http_status(:unprocessable_entity) }
+    end
+
+    context 'when called without a password' do
+      let(:attributes) { { email: 'someone@email.com' } }
+
+      it { is_expected.to have_http_status(:unprocessable_entity) }
+    end
+
+    context 'when called without a confirm password' do
+      let(:attributes) { { email: 'someone@email.com', password: '1234567@aM' } }
+
+      it { is_expected.to have_http_status(:unprocessable_entity) }
+    end
+
+    context 'when called with all necessary fields' do
+      let(:attributes) do
+        {
+          email: 'someone@email.com',
+          password: '1234567@aM',
+          password_confirmation: '1234567@aM'
+        }
+      end
+
+      it { is_expected.to have_http_status(:created) }
+    end
+  end
+
+  describe 'GET /profile' do
     let(:headers) { {} }
 
     before { get profile_path, headers: headers }
