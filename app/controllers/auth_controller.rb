182dd4fb62a88669d013fb08f2a486a891db574a
sanevillain <denis.ismail@code.berlin>
2022-04-15 11:01:48 +0200

    Use http-only cookies on refresh_token route

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -31,18 +31,18 @@ class AuthController < ApplicationController
   end
 
   def refresh_token
-    token = refresh_token_params[:refresh_token]
-
-    decoded_refresh_token = Auth.decode_refresh_token(token)
-    user = User.find_by(refresh_token: decoded_refresh_token['refresh_token'])
+    token = decode_refresh_token_from_params || decode_refresh_token_from_cookie
+    user = User.find_by(refresh_token: token['refresh_token'])
 
     raise InvalidRefreshTokenError if user.nil?
 
     tokens, base64_string = issue_tokens(user)
     user.update(refresh_token: base64_string)
 
+    auth_cookies_set(tokens)
+
     render json: { data: { attributes: tokens } }
-  rescue JWT::DecodeError, InvalidRefreshTokenError
+  rescue InvalidRefreshTokenError
     render status: :unauthorized
   end
 
@@ -83,6 +83,18 @@ class AuthController < ApplicationController
     refresh_token = Auth.issue_refresh_token(refresh_token: base64_string)
 
     [{ access_token: access_token, refresh_token: refresh_token }, base64_string]
+  end
+
+  def decode_refresh_token_from_params
+    Auth.decode_refresh_token(refresh_token_params[:refresh_token])
+  rescue JWT::DecodeError
+    nil
+  end
+
+  def decode_refresh_token_from_cookie
+    Auth.decode_refresh_token(cookies.encrypted[:refresh_token])
+  rescue JWT::DecodeError
+    nil
   end
 
   class InvalidRefreshTokenError < StandardError; end
